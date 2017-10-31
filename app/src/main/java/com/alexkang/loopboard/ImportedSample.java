5808190f90cd908c078164586ef7b9003bd7941b
Alex Kang <akang95@gmail.com>
2017-10-31 00:36:49 -0700

    Fixed imported sample bugs with playback

--- app/src/main/java/com/alexkang/loopboard/ImportedSample.java
+++ app/src/main/java/com/alexkang/loopboard/ImportedSample.java
@@ -8,21 +8,26 @@ import java.io.File;
 
 class ImportedSample extends Sample {
 
-    private final String name;
-    private final MediaPlayer mediaPlayer;
+    private final Context context;
+    private final File sampleFile;
 
-    ImportedSample(Context context, File file) {
-        name = file.getName();
-        mediaPlayer = MediaPlayer.create(context, Uri.parse(file.getAbsolutePath()));
+    private MediaPlayer mediaPlayer;
+
+    ImportedSample(Context context, File sampleFile) {
+        this.context = context;
+        this.sampleFile = sampleFile;
     }
 
     @Override
     String getName() {
-        return name;
+        return sampleFile.getName();
     }
 
     @Override
     synchronized void play(boolean isLooped) {
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(context, Uri.parse(sampleFile.getAbsolutePath()));
+        }
         mediaPlayer.setLooping(isLooped);
         mediaPlayer.seekTo(0);
         mediaPlayer.start();
@@ -30,17 +35,21 @@ class ImportedSample extends Sample {
 
     @Override
     synchronized void stop() {
-        mediaPlayer.pause();
-        mediaPlayer.setLooping(false);
+        if (mediaPlayer == null) {
+            return;
+        }
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        mediaPlayer = null;
     }
 
     @Override
     synchronized boolean isLooping() {
-        return mediaPlayer.isLooping();
+        return mediaPlayer != null && mediaPlayer.isLooping();
     }
 
     @Override
     synchronized void shutdown() {
-        mediaPlayer.release();
+        stop();
     }
 }
